880260478a34af75374263ecc37655f557eb5855
GhostCodeur <jpdevweb@gmail.com>
2021-06-01 08:52:56 +0200

    Dépôt des exercices TDD

---
+++ spec/questions.spec.js
@@ -0,0 +1,48 @@
+describe('Les chaines de caractères : ', function () {//on décrit la fonctionnalité
+  // "it" indique le comportement attendu
+    it('Donner la taille d\'une chaine de caractères', function () {
+      // on crée un exemple
+        var result = tailleString('Ce texte à une certaine taille');
+        // on donne le reésultat attendu
+        expect(result).toEqual(30);
+    });
+
+    // it('Remplacer le premier e d\'une chaine par un espace', function () {
+    //
+    // });
+    //
+    //   it('Concatener deux chaines de caractères', function () {
+    //
+    //   });
+    //
+    // it('Afficher le cinquième caractère d\'une chaine', function () {
+    //
+    // });
+    //
+    // it('Afficher les 9 premiers caractères d\une chaîne de caractère', function () {
+    //
+    // });
+    //
+    // it('Mettre en majuscule la chaine', function () {
+    //
+    // });
+    //
+    // it('Mettre en minuscule la chaine', function () {
+    //
+    // });
+    // it('Supprimer les espaces avant et après la chaine', function () {
+    //
+    // });
+    // it('Afficher true si le parametre d\'entrée de la fonction est de type string', function () {
+    //
+    // });
+    // it('Afficher l\'extension du fichier', function () {
+    //
+    // });
+    // it('Compter le nombre d\'espace dans la chaine', function () {
+    //
+    // });
+    // it('Inverser une chaine de caractères', function () {
+    //
+    // });
+})
---
+++ src/questions.js
@@ -0,0 +1,5 @@
+/*
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
